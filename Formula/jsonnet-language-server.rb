743b8aba1cc188595ebc30cdd88f97907c9fa212
Liza Chevalier <lizalc@pm.me>
2022-01-18 23:15:27 -0500

    add jsonnet language server pt2

--- Formula/jsonnet-language-server.rb
+++ Formula/jsonnet-language-server.rb
@@ -3,11 +3,14 @@ class JsonnetLanguageServer < Formula
 
   desc "A Language Server Protocol (LSP) server for Jsonnet"
   homepage "https://github.com/jdbaldry/jsonnet-language-server"
-  url "https://github.com/jdbaldry/jsonnet-language-server/releases/download/v#{version}/jsonnet-language-server"
-  sha256 "82e106efb3b613bb6a8726d343cea148e8a3bb23c55876421dcc9dd72d219560"
+  url "https://github.com/jdbaldry/jsonnet-language-server/archive/v#{version}.tar.gz"
+  sha256 "a3baa21096a387561766acb34a8855c7f0b441f8f4cd84a0ae6b9a01b2cf040d"
   license "AGPL-3.0"
+  head "https://github.com/jdbaldry/jsonnet-language-server.git", branch: "main"
+
+  depends_on "go" => :build
 
   def install
-    bin.install "jsonnet-language-server"
+    system "go", "build", "-o", bin/"jsonnet-language-server"
   end
 end
