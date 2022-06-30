6ccb1229aaec61434d2c680582518ae75dfa289b
Liza Chevalier <lizalc@pm.me>
2022-06-30 11:46:46 -0400

    chore: switch jsonnet-lsp repo, bump version
    Now uses repo linked to in nvim-lspconfig server configurations

--- Formula/jsonnet-language-server.rb
+++ Formula/jsonnet-language-server.rb
@@ -1,12 +1,12 @@
 class JsonnetLanguageServer < Formula
-  version "0.3.0"
+  version "0.7.2"
 
-  desc "A Language Server Protocol (LSP) server for Jsonnet"
-  homepage "https://github.com/jdbaldry/jsonnet-language-server"
-  url "https://github.com/jdbaldry/jsonnet-language-server/archive/v#{version}.tar.gz"
-  sha256 "a3baa21096a387561766acb34a8855c7f0b441f8f4cd84a0ae6b9a01b2cf040d"
+  desc "A Language Server Protocol (LSP) server for Jsonnet (https://jsonnet.org)"
+  homepage "https://github.com/grafana/jsonnet-language-server"
+  url "https://github.com/grafana/jsonnet-language-server/archive/v#{version}.tar.gz"
+  sha256 "6ec1021db34334fd21f738e3c2798250a26c1a4facebf5fb10ff6d9f5ad62f3e"
   license "AGPL-3.0"
-  head "https://github.com/jdbaldry/jsonnet-language-server.git", branch: "main"
+  head "https://github.com/grafana/jsonnet-language-server", branch: "main"
 
   depends_on "go" => :build
 
